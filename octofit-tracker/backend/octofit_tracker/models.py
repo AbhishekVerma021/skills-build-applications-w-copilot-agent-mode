75fa02ab5ce02ec1b42c5b547e9eff57ced8d0da
AbhishekVerma021 <abhishek.ad.verma@accenture.com>
2025-12-11 16:00:36 +0000

    up

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,19 +1,25 @@
 from djongo import models
 
+
 class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100, unique=True)
     class Meta:
         db_table = 'teams'
 
+
 class User(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members', to_field='id')
     class Meta:
         db_table = 'users'
 
+
 class Activity(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities')
+    id = models.ObjectIdField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='activities', to_field='id')
     type = models.CharField(max_length=100)
     duration = models.IntegerField()  # minutes
     calories = models.IntegerField()
@@ -21,15 +27,19 @@ class Activity(models.Model):
     class Meta:
         db_table = 'activities'
 
+
 class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
     name = models.CharField(max_length=100)
     description = models.TextField()
     suggested_for = models.CharField(max_length=100)
     class Meta:
         db_table = 'workouts'
 
+
 class Leaderboard(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='leaderboard')
+    id = models.ObjectIdField(primary_key=True, editable=False)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='leaderboard', to_field='id')
     points = models.IntegerField()
     class Meta:
         db_table = 'leaderboard'
